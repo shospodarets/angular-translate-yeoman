b33e4ae9b8a583cbdb33fc0d265f0ae187a21080
shospodarets <shospodarets@guidewire.com>
2015-02-09 18:52:55 +0000

    Cleaning

--- app/scripts/services/LocaleService.js
+++ app/scripts/services/LocaleService.js
@@ -8,13 +8,10 @@
 angular.module('translateApp')
   .service('LocaleService', function ($translate, LOCALES, $rootScope) {
     'use strict';
-
-    // _LOCALES
-
-    // vars
+    // VARS
     var localesObj = LOCALES.locales;
 
-    // get locales and locales display names
+    // locales and locales display names
     var _LOCALES = Object.keys(localesObj);
     if (!_LOCALES || _LOCALES.length === 0) {
       console.error('There are no _LOCALES provided');
@@ -24,17 +21,12 @@ angular.module('translateApp')
       _LOCALES_DISPLAY_NAMES.push(localesObj[locale]);
     });
 
-    var currentLocale = null;
+    var currentLocale = $translate.proposedLanguage();// because of async loading
 
-    // methods
+    // METHODS
     var checkLocaleIsValid = function (locale) {
       return _LOCALES.indexOf(locale) !== -1;
     };
-
-    $rootScope.$on('$translateChangeSuccess', function (event, data) {
-      // set "lang" attribute to html
-      document.documentElement.setAttribute('lang', data.language);
-    });
 
     var setLocale = function (locale) {
       if (!checkLocaleIsValid(locale)) {
@@ -45,18 +37,16 @@ angular.module('translateApp')
       $translate.use(locale);
     };
 
-    currentLocale = $translate.proposedLanguage();// because of async loading
+    // EVENTS
+    $rootScope.$on('$translateChangeSuccess', function (event, data) {
+      // set "lang" attribute to html
+      document.documentElement.setAttribute('lang', data.language);
+    });
 
     return {
-      //getLocale: function () {
-      //  return currentLocale;
-      //},
       getLocaleDisplayName: function () {
         return localesObj[currentLocale];
       },
-      //setLocale: function (locale) {
-      //  setLocale(locale);
-      //},
       setLocaleByDisplayName: function (localeDisplayName) {
         setLocale(
           _LOCALES[
@@ -64,9 +54,6 @@ angular.module('translateApp')
             ]
         );
       },
-//        getLocales: function () {
-//            return _LOCALES;
-//        },
       getLocalesDisplayNames: function () {
         return _LOCALES_DISPLAY_NAMES;
       }
